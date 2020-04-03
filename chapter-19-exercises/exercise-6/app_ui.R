2f38f381b49706300a6b0e31db290e42bea3c80c
mdabrowski <micdab90@gmail.com>
2020-04-03 22:23:51 +0200

    Add solution of exercise 6

--- chapter-19-exercises/exercise-6/app_ui.R
+++ chapter-19-exercises/exercise-6/app_ui.R
@@ -6,8 +6,16 @@ library(shiny)
 ui <- fluidPage(
   mainPanel(
     # Add a selectInput that allows you to select a variable to map
+    selectInput(
+      inputId = "map_var",
+      label = "Variable to Map", 
+      choices = list("Population" = "population", 
+                     "Electoral Votes" = "votes", 
+                     "Votes per 100k Population" = "ratio"
+                     )
+    ),
     
     # Use `plotlyOutput()` to show your map
-    
+    plotlyOutput(outputId = "map")
   )
 )
