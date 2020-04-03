9a72e93a1f3e02575ff81ea9017bd9fb051de6f2
mdabrowski <micdab90@gmail.com>
2020-04-03 21:56:30 +0200

    Add solution of exercise 5

--- chapter-19-exercises/exercise-5/app_server.R
+++ chapter-19-exercises/exercise-5/app_server.R
@@ -6,3 +6,12 @@ library(ggplot2)
 
 # It should use an `input` with features: `x_var`, `y_var`, `color`, and `size`
 # Save the result of `renderPlot` to output$scatter
+server <- function(input, output) {
+  output$scatter <- renderPlot({
+    ggplot(data = mpg) +
+      geom_point(mapping = aes_string(x = input$x_var, y = input$y_var),
+                 color = input$color,
+                 size = input$size
+      )
+  })
+}
