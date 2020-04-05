3a049df9f2fb618258bf6a7648d04f97d2fa9d86
mdabrowski <micdab90@gmail.com>
2020-04-05 20:57:19 +0200

    Add solution of exercise 7

--- chapter-19-exercises/exercise-7/app_server.R
+++ chapter-19-exercises/exercise-7/app_server.R
@@ -2,21 +2,28 @@
 library(dplyr)
 
 # Read in data
-source('./scripts/build_map.R')
-source('./scripts/build_scatter.R')
-df <- read.csv('./data/electoral_college.csv', stringsAsFactors = FALSE)
-state_codes <- read.csv('./data/state_codes.csv', stringsAsFactors = FALSE)
+source('scripts/build_map.R')
+source('scripts/build_scatter.R')
+df <- read.csv('data/electoral_college.csv', stringsAsFactors = FALSE)
+state_codes <- read.csv('data/state_codes.csv', stringsAsFactors = FALSE)
 
 # Join together state.codes and df
-joined_data <- left_join(df, state_codes, by="state")
+joined_data <- left_join(df, state_codes, by = "state")
 
 # Compute the electoral votes per 100K people in each state
-joined_data <- joined_data %>% mutate(ratio = votes/population * 100000)
+joined_data <- joined_data %>% 
+  mutate(ratio = votes / population * 100000)
 
 # Define your shiny server in which you...
+server <- function(input, output) {
   
   # Render a plotly object that returns your map
-  
+  output$map <- renderPlotly({
+    return(build_map(joined_data, input$map_var))
+  })
   
   # Render a plotly object that returns your scatter plot
-  
+  output$scatter <- renderPlotly({
+    return(build_scatter(joined_data, input$search))
+  })
+}
--- chapter-19-exercises/exercise-7/app_ui.R
+++ chapter-19-exercises/exercise-7/app_ui.R
@@ -4,40 +4,85 @@ library(plotly)
 
 # Define a variable `map_sidebar_content` that is a `sidebarPanel()` for your
 # first (map) page that contains:
+map_sidebar_content <- sidebarPanel(
+  
   # An input to select variable to map
+  selectInput(
+    inputId = "map_var",
+    label = "Variable to Map",
+    choices = list(
+      "Population" = "population",
+      "Electoral Votes" = "votes",
+      "Votes / Population" = "ratio"
+    )
+  )
+)
 
 # Define a variable `map_main_content` that is a `mainPanel()` for your
 # first (map) page that contains the `plotlyOutput()` of your map
+map_main_content <- mainPanel(
+  plotlyOutput(outputId = "map")
+)
 
 # Create a variable `map_panel` that stores a `tabPanel()` for your first page
 # It should include the following:
+map_panel <- tabPanel(
+  "Map",
+  
+  # Add a titlePanel to your tab
+  titlePanel("Electoral College Votes"),
+  
   # A `sidebarLayout()` that contains...
-    
+  sidebarLayout(
+
     # Your `map_sidebar_content`
+    map_sidebar_content,
     
     # Your `map_main_content`
-
+    map_main_content
+  )
+)
 
 # Define a variable `scatter_sidebar_content` that is a `sidebarPanel()` for 
 # your second (scatter) page that contains:
 # - a `textInput()` widget for searching for a state in your scatter plot
-
+scatter_sideband_content <- sidebarPanel(
+  textInput(
+    inputId = "search",
+    label = "Find a State",
+    value = ""
+  )
+)
 
 # Define a variable `scatter_main_content` that is a `mainPanel()` for your
 # second (scatter) page that contains the `plotlyOutput()` of your scatter
-
+scatter_main_content <- mainPanel(
+  plotlyOutput(outputId = "scatter")
+)
 
 # Create a variable `scatter_panel` that stores a `tabPanel()` for your 2nd page
 # It should include the following:
+scatter_panel <- tabPanel(
+  "Scatter",
   
   # Add a titlePanel to your tab
+  titlePanel("Population v.s. Vote Power"),
   
   # Create a sidebar layout for this tab (page)
+  sidebarLayout(
     
     # Display your `scatter_sidebar_content`
-    
+    scatter_sideband_content,
     
     # Display your `scatter_main_content`
+    scatter_main_content
+  )
+)
     
 # Define a variable `ui` storing a `navbarPage()` element containing
 # your two pages defined above
+ui <- navbarPage(
+  "Electoral College",
+  map_panel,
+  scatter_panel
+)
